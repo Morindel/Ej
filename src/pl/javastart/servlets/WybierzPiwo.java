a7e919560f4481ab9e670c3981f7a972825fb764
Jakub Kołodziej <kolomakuba@gmail.com>
2017-02-20 01:05:21 +0100

    JSP

--- src/pl/javastart/servlets/WybierzPiwo.java
+++ src/pl/javastart/servlets/WybierzPiwo.java
@@ -1,9 +1,8 @@
 package pl.javastart.servlets;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Iterator;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,20 +12,25 @@ import com.example.model.EkspertPiwny;
  
 public class WybierzPiwo extends HttpServlet {
     public void doPost(HttpServletRequest request,HttpServletResponse response)throws IOException , ServletException {
-    	response.setContentType("text/html");
-    	PrintWriter out = response.getWriter();
-    	out.println("Porada piwna<br>");
+    	/*response.setContentType("text/html");
+    	PrintWriter out = response.getWriter()*/;
+    	/*out.println("Porada piwna<br>");
+    	*/
     	String c = request.getParameter("kolor");
-    	out.println("<br>Wybrany kolor piwa : " + c);
-    	
+    	/*out.println("<br>Wybrany kolor piwa : " + c);
+    	*/
     	EkspertPiwny p1=new EkspertPiwny();
     	List wynik = p1.getMarki("kolor");
     	
-    	Iterator it = wynik.iterator();
+    	request.setAttribute("styles", wynik);
+    	RequestDispatcher view = request.getRequestDispatcher("Wynik.jsp");
+    	view.forward(request, response);
+    	
+    	/*Iterator it = wynik.iterator();
     	while(it.hasNext())
     	{
     		out.println("<br >Sprobój : "  + it.next() );
-    	}
+    	}*/
     	//out.print("Piwa ktore polubisz to" + p1.getMarki("kolor"));
     }
 }
